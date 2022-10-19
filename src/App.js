8eb65123e16ea8988845994b5b7cbd17a55c39d0
Devin Li <51217000+butterriolu@users.noreply.github.com>
2022-10-19 18:26:30 -0400

    added clock

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Clock from "./Clock";
 
 function App() {
+  const user = {
+    first_name: "Devin",
+    last_name: "Li",
+  };
+  function format(user) {
+    return `${user.first_name} ${user.last_name}`;
+  }
+  const greet = <h1>{`Hello, ${format(user)}`}</h1>;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {greet}
+      <Clock />
     </div>
   );
 }
